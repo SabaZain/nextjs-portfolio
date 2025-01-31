93b2d2c96a5a675c1a98b7903feafbde4d875294
Saba Zain <saba_punjwani@yahoo.com>
2025-01-31 21:55:56 +0500

    updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiMail } from "react-icons/ci";
 import { FaPhoneAlt } from "react-icons/fa";
 
+
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    setResponseMessage(`Thankyou, ${name}! Your message has been sent.`);
+
+    setName('');
+    setEmail('');
+    setMessage('');
+};
+
   return (
     <div id='contact' className='pt-32 container'>
       <div className='grid md:grid-cols-2 gap-10'>
@@ -19,30 +34,53 @@ const Contact = () => {
         <div className='space-y-8'>
             <div className='flex flex-col gap-1' data-aos="zoom-in-up">
                 <label htmlFor='name'>Name</label>
-                <input type="text"
+                <input 
+                type="text"
                 className='h-[40px] bg-transparent border border-slate-500'
-                id='name'/>
+                id='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
             </div>
 
             <div className='flex flex-col gap-1' data-aos="zoom-in-up">
                 <label htmlFor='email'>Email</label>
-                <input type="text"
+                <input 
+                type="text"
                 className='h-[40px bg-transparent border border-slate-500'
-                id='email'/>
+                id='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
 
             <div className='flex flex-col gap-1' data-aos="zoom-in-up">
                 <label htmlFor='msg'>Message</label>
                 <textarea 
                 className='bg-transparent border border-slate-500'
-                id='msg' rows={6}>
-                </textarea>
+                id='msg' 
+                rows={6}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                />
             </div>
-            <button className='bg-green-800 text-xl p-2 border rounded-sm' data-aos="zoom-in-up">Send</button>
+            <button 
+            onClick={handleSubmit}
+            className='bg-green-800 text-xl p-2 border rounded-sm' 
+            data-aos="zoom-in-up"
+            >
+            Send
+            </button>
+            {
+              responseMessage && (
+                <div className='mt-4 text-green-800'>
+                  {responseMessage}
+                </div>
+              )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
